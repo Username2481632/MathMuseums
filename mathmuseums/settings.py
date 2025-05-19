c222ecfe890158632ac56e479d80d0f148a26a80
micha@mineframe <michael.moshchuk@gmail.com>
2025-05-19 14:34:54 +0000

    Move ALLOWED_HOSTS to .env and update settings.py to use env.list('ALLOWED_HOSTS')

--- mathmuseums/settings.py
+++ mathmuseums/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.helioho.st']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
