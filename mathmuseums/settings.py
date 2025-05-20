519cc474078049a5bea822b0482f1a60042a69d6
micha@mineframe <michael.moshchuk@gmail.com>
2025-05-20 14:22:33 +0000

    Add EMAIL_USE_SSL to production email settings for flexible SMTP configuration

--- mathmuseums/settings.py
+++ mathmuseums/settings.py
@@ -185,6 +185,7 @@ else:
     EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
     EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+    EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
 
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='noreply@mathmuseums.example.com')
 
