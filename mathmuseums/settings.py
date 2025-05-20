41be7346b542b1d2d50ab46713a24acd2bfcecd6
micha@mineframe <michael.moshchuk@gmail.com>
2025-05-20 15:25:55 +0000

    Remove all default values from email config; require explicit .env settings for production mail

--- mathmuseums/settings.py
+++ mathmuseums/settings.py
@@ -180,14 +180,14 @@ if DEBUG:
 else:
     # Use SMTP backend for production
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST', default='localhost')
-    EMAIL_PORT = env.int('EMAIL_PORT', default=587)
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
-    EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
-    EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_PORT = env.int('EMAIL_PORT')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+    EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
 
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='noreply@mathmuseums.example.com')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 AUTH_USER_MODEL = 'authentication.User'
 
